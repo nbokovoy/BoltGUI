44cb970147a2d7401b86b387a168b5ff90ac6730
Nikita <nikita.bokovoy@gmail.com>
2015-07-31 16:14:31 +0300

    basic functionaity

---
+++ boltgui.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/boltdb/bolt"
+)
+
+var (
+	dbpath = "boltdb.db"
+)
+
+func main() {
+	if len(os.Args) > 1 {
+		dbpath = os.Args[1]
+	}
+
+	http.HandleFunc("/exit", exit)
+	http.HandleFunc("/getBuckets", getBucketsHandler)
+	http.HandleFunc("/getEntries", getEntriesHandler)
+	http.HandleFunc("/delEntry", delEntryHandler)
+	http.HandleFunc("/delBucket", delBucketHandler)
+	http.HandleFunc("/setEntry", setEntryHandler)
+	http.HandleFunc("/setBucket", setBucketHandler)
+
+	http.Handle("/", http.FileServer(http.Dir("/home/nikita/go/src/BoltGUI/html")))
+	http.ListenAndServe(":8080", nil)
+}
+
+func delEntryHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	delEntry(r.FormValue("bucket"), r.FormValue("key"))
+}
+
+func delBucketHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	delBucket(r.FormValue("bucket"))
+}
+
+func setEntryHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	setEntry(r.FormValue("bucket"), r.FormValue("key"), r.FormValue("value"))
+}
+
+func setBucketHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r)
+	setBucket(r.FormValue("bucket"))
+}
+
+func getBucketsHandler(w http.ResponseWriter, r *http.Request) {
+	buckets := getBuckets()
+
+	js, err := json.Marshal(buckets)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func getEntriesHandler(w http.ResponseWriter, r *http.Request) {
+	entries := getEntries(r.URL.Query().Get("buck"))
+
+	js, err := json.Marshal(entries)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func exit(w http.ResponseWriter, r *http.Request) {
+	os.Exit(0)
+}
+
+func delEntry(bucket, key string) {
+	db, err := bolt.Open(dbpath, 0600, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		buck := tx.Bucket([]byte(bucket))
+		return buck.Delete([]byte(key))
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func delBucket(bucket string) {
+	db, err := bolt.Open(dbpath, 0600, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		return tx.DeleteBucket([]byte(bucket))
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func setEntry(bucket, key, value string) {
+	db, err := bolt.Open(dbpath, 0600, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		buck := tx.Bucket([]byte(bucket))
+		return buck.Put([]byte(key), []byte(value))
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func setBucket(bucket string) {
+	db, err := bolt.Open(dbpath, 0600, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte(bucket))
+		return err
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getEntries(bucket string) map[string]string {
+	db, err := bolt.Open(dbpath, 0600, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	entriesMap := map[string]string{}
+
+	db.View(func(tx *bolt.Tx) error {
+		curBucket := tx.Bucket([]byte(bucket))
+
+		curBucket.ForEach(func(k, v []byte) error {
+			entriesMap[string(k)] = string(v)
+			return nil
+		})
+		return nil
+	})
+	return entriesMap
+}
+
+func getBuckets() []string {
+	db, err := bolt.Open(dbpath, 0600, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	bucketsList := []string{}
+	db.View(func(tx *bolt.Tx) error {
+		tx.ForEach(func(name []byte, b *bolt.Bucket) error {
+			bucketsList = append(bucketsList, string(name))
+			return nil
+		})
+		return nil
+	})
+	return bucketsList
+}
---
+++ html/js/main.js
@@ -0,0 +1,236 @@
+$(document).ready(function() {
+  getBuckets();
+
+  $(document).on('click', ".bucket h4", function() {
+    getBucketEnties(this);
+     $(this).parent().children(".collapse").toggle(100);
+  });
+
+  $(document).on('dblclick', ".editable", function() {
+    if (!$(this).hasClass("editing")) {
+      startEditing(this);
+    }
+  });
+
+  $(document).on('click', ".del", function() {
+    var row = $(this).parent();
+    deleteBucketEntry(getBucket(row), getKey(row));
+    $(row).hide(200);
+  });
+  $(document).on('click', ".del-bucket", function(){
+  	var bucket = getBucket(this);
+  	deleteBucket(bucket);
+  	$(this).parent().remove();
+  });
+  
+  $(document).on('click', ".createEntry", function(){
+    var table = $(this).parent().find("table");
+    var newRow = $("<tr class='editable'><td class='del'>Del</td><td><div class='key'>New key here</div></td><td><div class='value'>New value here</div></td></tr>");
+    $("tr:first", table).after(newRow);
+    startEditing(newRow);
+  });
+
+
+	$(document).on("submit", "#new-bucket-form", function(){
+            $.post( '/setBucket', {bucket: $("#bucket").val()}, function(res){
+                console.log(res);
+            });
+        });
+});
+
+function fillEntries(bucket, entries) {
+  var buckName = $(bucket).text();
+
+  var container = $("<div></div>").addClass("collapse");
+  var createButton = $("<button>").addClass("createEntry btn btn-primary").text("New entry");
+  $(container).append(createButton);
+  var table = $("<table></table>").addClass("table");
+
+  $(table).append("<tr><th/><th>Key</th><th>Value</th></tr>");
+
+  for (var key in entries) {
+    if (entries.hasOwnProperty(key)) {
+    	var value
+    	try {
+    		value = JSON.parse(entries[key]);
+    		value = JSON.stringify(value ,null, '\t');
+  		}
+  		catch (e) {
+    		console.log("error: "+e);
+    		value = entries[key]
+  		};
+    		
+
+      $(table).append("<tr class='editable'><td class='del'>Del</td><td><div class='key'>" + key + "</div></td><td><div class='value'>" + value + "</div></td></tr>");
+
+    }
+  }
+  $(table).appendTo(container);
+  $(bucket).parent().append(container);
+
+  $(bucket).parent().children(".collapse").toggle(100);
+}
+
+function fillBuckets(buckets) {
+  buckets.forEach(function(bucket, index, array) {
+    //Create main container for bucket
+    $("body").append("<div class=\"bucket\"><div class='del-bucket btn btn-xs'></div><h4>" + bucket + "</h4></div>");
+  });
+}
+
+function getBuckets() {
+  $.getJSON('/getBuckets', function(data){
+
+		fillBuckets(data);
+});
+}
+
+function getBucketEnties(bucket) {
+	if ($(bucket).parent().has(".collapse").length) {
+    return;
+  }
+	$.getJSON('/getEntries',{buck: $(bucket).text()}, function(data){
+
+		fillEntries(bucket, data);
+});
+}
+
+function deleteBucket(bucket){
+	$.post('/delBucket',{bucket:bucket}, function( data ) {
+});
+}
+
+function deleteBucketEntry(bucket, key) {
+	$.post('/delEntry',{bucket:bucket,key:key}, function( data ) {
+});
+  alert("Deleting from " + bucket + " key: " + key);
+}
+
+function saveBucketEntry(bucket, key, value) {
+  $.post('/setEntry',{bucket:bucket,key:key,value:value}, function( data ) {
+});
+}
+
+function startEditing(row) {
+  stopEditing();
+  $(row).addClass("editing");
+
+  var key = getKey(row);
+  var value = getValue(row);
+
+  //Saving old values
+  addHidden(row, key, value);
+
+  //Show editable textarea
+  showEditable(row);
+
+  showButtos(row);
+}
+
+function showEditable(row) {
+  //Geting current values
+  var key = $(row).find(".old-key").val();
+  var value = $(row).find(".old-value").val();
+
+  //Create textarea
+  var editableKey = $("<textarea />");
+  var editableValue = $("<textarea />");
+  editableKey.val(key);
+  editableValue.val(value);
+  editableKey.addClass("key");
+  editableValue.addClass("value");
+
+  //making fields editable
+  $(row).find(".key").replaceWith(editableKey);
+  $(row).find(".value").replaceWith(editableValue);
+  editableValue.focus();
+}
+
+function showButtos(row) {
+  var saveButton = $('<input/>', {
+    type: "button",
+    class: "btn btn-success save-btn",
+    value: "Save"
+  });
+  var cancelButton = $('<input/>', {
+    type: "button",
+    class: "btn btn-default cancel-btn",
+    value: "Cancel"
+  });
+
+  saveButton.click(function() {
+    var row = $(this).parent();
+    var key = getKey(row);
+    var value = getValue(row);
+    var bucket = getBucket(row);
+
+    $(row).find(".old-key").val(key);
+    $(row).find(".old-value").val(value);
+    saveBucketEntry(bucket, key, value);
+
+    stopEditing();
+  })
+
+  cancelButton.click(function() {
+    stopEditing();
+  })
+
+  $(row).append(saveButton);
+  $(row).append(cancelButton);
+
+}
+
+function addHidden(row, key, value) {
+  $('<input/>', {
+    type: 'hidden',
+    class: 'old-key',
+    value: key
+  }).appendTo(row);
+  $('<input/>', {
+    type: 'hidden',
+    class: 'old-value',
+    value: value
+  }).appendTo(row);
+}
+
+function stopEditing() {
+  var row = $(".editing");
+    //Geting current values, it will be new if Save button pressed
+  var key = $(row).find(".old-key").val();
+  var value = $(row).find(".old-value").val();
+
+  var viewKey = $("<div>");
+  var viewValue = $("<div>");
+  viewKey.html(key);
+  viewValue.html(value);
+  viewKey.addClass("key");
+  viewValue.addClass("value");
+
+  $(row).find(".key").replaceWith(viewKey);
+  $(row).find(".value").replaceWith(viewValue);
+
+  $(row).find(".save-btn").remove();
+  $(row).find(".cancel-btn").remove();
+
+  $(row).removeClass("editing");
+}
+
+function getKey(row) {
+  var field = $(row).find(".key");
+  if ($(field).val() != "")
+    return $(field).val();
+
+  return field.text();
+}
+
+function getValue(row) {
+  var field = $(row).find(".value");
+  if ($(field).val() != "")
+    return $(field).val();
+
+  return field.text();
+}
+
+function getBucket(row) {
+  return $(row).parents(".bucket").find("h4").text();
+}
